c4838e9cf0029711333b9510febe0f7a4e635987
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-10-08 11:55:58 +0530

    added postsReducer

--- src/reducres/index.js
+++ src/reducres/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
-export default combineReducers({ replaceMe: () => 10 });
+export default combineReducers({ posts: postsReducer });
