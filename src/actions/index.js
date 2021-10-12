38f860136d1be50d05b848fe6b696454b2773db9
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-10-12 18:50:10 +0530

    Added fetchPostsAndUsers action creator to debug over fetching issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// Added fetchPostsAndUsers to debug the overfetching of users problem
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    // Dispatching the fetchPosts action creator and waiting till all the posts are fetched
+    await dispatch(fetchPosts());
+    // Fetching all the unique userIds from the posts using lodash
+    const userIds = _.uniq(_.map(getState().posts, "userId"));
+    // Dispatching fetchUser action creator to fetch all the users
+    userIds.forEach((id) => dispatch(fetchUser(id)));
+  };
+};
 
 export const fetchPosts = () => {
   return async (dispatch) => {
